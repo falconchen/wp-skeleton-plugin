8356cd33365a4c57c5d142893173e04a5d144cc0
falcon <falcon_chen@qq.com>
2021-05-19 11:40:17 +0800

    update autoload

--- autoload.php
+++ autoload.php
@@ -1,14 +1,13 @@
 <?php
  /**
- * Plugin Name: WP Skeleton Plugin
- * Plugin URI: https://github.com/ptahdunbar/wp-skeleton-plugin
- * Description: A walking skeleton WordPress plugin. -_O
- * Version: 0.0.1
- * Author: Ptah Dunbar
- * Author URI: http://ptahdunbar.com
- * Text Domain: wp-skeleton-plugin
+ * Plugin Name: lms helper
+ * Plugin URI: https://github.com/falconchen/wp-skeleton-plugin.git 
+ * Description: lms plugin for mpw
+ * Version: 0.1
+ * Author: Falcon
+ * Author URI: https://www.cellmean.com
+ * Text Domain: lms 
  * Domain Path: languages/
- * GitHub Plugin URI: <owner>/<repo>
  * GitHub Branch: master
  * License: GPL2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
